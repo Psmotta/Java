ab6ed2d4ed2b1e985e0ca4ee95e659665fb993dd
Pedro Motta <55262114+Psmotta@users.noreply.github.com>
2022-08-09 21:26:31 -0300

    Add files via upload

---
+++ Programação Modular/Revisão/Exercicio5/src/App.java
@@ -0,0 +1,13 @@
+import java.io.FileNotFoundException;
+
+public class App {
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        Sistema sistema = new Sistema();
+
+        System.out.println(sistema.getVetores());
+
+    }
+
+}
