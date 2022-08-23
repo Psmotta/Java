47aabd893ca1d6dae13501daa2385b6d9d89fcb5
Pedro Motta <55262114+Psmotta@users.noreply.github.com>
2022-08-23 15:58:58 -0300

    Add files via upload

---
+++ Programação Modular/Microondas/Microondas/src/Luz.java
@@ -0,0 +1,13 @@
+public class Luz {
+
+    private boolean luz_Microondas;
+
+    public void ascenderLuz(){
+        luz_Microondas=true;
+    }
+
+    public void desligarLuz(){
+        luz_Microondas=false;
+    }
+
+}
