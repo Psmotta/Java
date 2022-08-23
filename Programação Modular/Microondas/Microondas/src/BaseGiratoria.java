a594af1705813afbafb548ded12320b3fa9e11c9
Pedro Motta <55262114+Psmotta@users.noreply.github.com>
2022-08-23 15:58:31 -0300

    Add files via upload

---
+++ Programação Modular/Microondas/Microondas/src/BaseGiratoria.java
@@ -0,0 +1,13 @@
+public class BaseGiratoria {
+
+    private boolean base_Giratoria;
+
+    public void girarBase(){
+        base_Giratoria=true;
+    }
+
+    public void desligarBase(){
+        base_Giratoria=false;
+    }
+
+}
